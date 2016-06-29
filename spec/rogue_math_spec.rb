0f648af765c5c31ee8eca3d0d09644cfdd95307c
mistertime <gurgitatorsupreme@gmail.com>
2016-06-28 21:00:41 -0500

    Fixed tests, README

--- spec/rogue_math_spec.rb
+++ spec/rogue_math_spec.rb
@@ -2,30 +2,30 @@ require_relative '../rogue_math.rb'
 
 describe RogueMath do
   describe 'add' do
-    let(:a) { 2 }
-    let(:b) { 2 }
+    let(:a) { 7 }
+    let(:b) { 9 }
 
-    specify { expect(RogueMath.add(a, b)).to eq 4 }
+    specify { expect(RogueMath.add(a, b)).to eq (a + b) }
   end
 
   describe 'subtract' do
-    let(:a) { 2 }
+    let(:a) { 6 }
     let(:b) { 2 }
 
-    specify { expect(RogueMath.subtract(a, b)).to eq 0 }
+    specify { expect(RogueMath.subtract(a, b)).to eq (a - b) }
   end
 
   describe 'multiply' do
-    let(:a) { 2 }
+    let(:a) { 5 }
     let(:b) { 2 }
 
-    specify { expect(RogueMath.multiply(a, b)).to eq 4 }
+    specify { expect(RogueMath.multiply(a, b)).to eq a * b }
   end
 
   describe 'divide' do
-    let(:a) { 2 }
+    let(:a) { 7 }
     let(:b) { 2 }
 
-    specify { expect(RogueMath.divide(a, b)).to eq 1 }
+    specify { expect(RogueMath.divide(a, b)).to eq a / b }
   end
 end
