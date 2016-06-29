fe7fa11b20c1d86714855d6e6c2467e64abf9310
mistertime <gurgitatorsupreme@gmail.com>
2016-06-28 21:11:10 -0500

    Tests are now robust beyond your wildest imaginings

--- spec/rogue_math_spec.rb
+++ spec/rogue_math_spec.rb
@@ -2,29 +2,29 @@ require_relative '../rogue_math.rb'
 
 describe RogueMath do
   describe 'add' do
-    let(:a) { 7 }
-    let(:b) { 9 }
+    let(:a) { Random.rand }
+    let(:b) { Random.rand }
 
     specify { expect(RogueMath.add(a, b)).to eq a + b }
   end
 
   describe 'subtract' do
-    let(:a) { 6 }
-    let(:b) { 2 }
+    let(:a) { Random.rand }
+    let(:b) { Random.rand }
 
     specify { expect(RogueMath.subtract(a, b)).to eq a - b }
   end
 
   describe 'multiply' do
-    let(:a) { 5 }
-    let(:b) { 2 }
+    let(:a) { Random.rand }
+    let(:b) { Random.rand }
 
     specify { expect(RogueMath.multiply(a, b)).to eq a * b }
   end
 
   describe 'divide' do
-    let(:a) { 7 }
-    let(:b) { 2 }
+    let(:a) { Random.rand }
+    let(:b) { Random.rand }
 
     specify { expect(RogueMath.divide(a, b)).to eq a / b }
   end
