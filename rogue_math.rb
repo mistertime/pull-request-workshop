7de095a9a25208d8d13e55df66717ec7719a93bf
mistertime <gurgitatorsupreme@gmail.com>
2016-06-28 21:01:44 -0500

    Fixed math methods

--- rogue_math.rb
+++ rogue_math.rb
@@ -1,17 +1,17 @@
 class RogueMath
   def self.add(a, b)
-    a + a
-  end
-
-  def self.subtract(a, b)
-    b - a
-  end
-
-  def self.multiply(a, b)
     a + b
   end
 
+  def self.subtract(a, b)
+    a - b
+  end
+
+  def self.multiply(a, b)
+    a * b
+  end
+
   def self.divide(a, b)
-    b / a
+    a / b
   end
 end
